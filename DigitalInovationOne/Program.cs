074403f631cb38a1b45b3fb52b45ad30229b483e
Rafael Moura <rafaelmoura2929@gmail.com>
2020-05-23 19:46:22 -0300

    Escrevendo primeiro programa em dotnet

--- DigitalInovationOne/Program.cs
+++ DigitalInovationOne/Program.cs
@@ -6,7 +6,11 @@ namespace DigitalInovationOne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num = 5;
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine($"Bem vindo ao curso de .net! {i}");
+            }
         }
     }
 }
