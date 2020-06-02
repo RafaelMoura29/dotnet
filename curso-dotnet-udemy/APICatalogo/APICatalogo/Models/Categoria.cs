27f6d8b445dacc4347690ed5170f5f5fe16f60dc
Rafael Moura <rafaelmoura2929@gmail.com>
2020-06-01 21:53:38 -0300

    Criando models para produto e categoria

---
+++ curso-dotnet-udemy/APICatalogo/APICatalogo/Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogo.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public int Nome { get; set; }
+        public int ImagemUrl { get; set; }
+    }
+}
